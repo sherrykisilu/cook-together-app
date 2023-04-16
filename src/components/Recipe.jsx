a3a8bf077cf8fc9cca56ff2defd8703b10a5e3cf
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-04-16 20:44:31 +0300

    made Recipe component

---
+++ src/components/Recipe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RecipeCard from './RecipeCard'
+
+const Recipe = () => {
+  
+  return (
+    <RecipeCard>
+      <h1 className="text-white hover:cursor-pointer">
+        BBQ Recipes
+      </h1>
+    </RecipeCard>
+  )
+}
+
+export default Recipe
